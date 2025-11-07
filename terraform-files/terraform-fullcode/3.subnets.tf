0720ae0d6f1e209192a1eeeeef2a3963d296b477
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-07 22:41:41 +0530

    Update 3.subnets.tf

--- terraform-files/terraform-fullcode/3.subnets.tf
+++ terraform-files/terraform-fullcode/3.subnets.tf
@@ -8,12 +8,12 @@ resource "aws_subnet" "subnet1" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.myvpc.id
   tags = {
-    Name = "Public-SN-1"
+    Name = "Public-SN-2"
   }
-  availability_zone       = "eu-north-1a"
-  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-north-1b"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
