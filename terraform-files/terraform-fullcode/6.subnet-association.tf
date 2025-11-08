184c8344f1d82b06258fdc57ce51b6644990cc01
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-08 15:13:21 +0530

    Delete terraform-files/terraform-fullcode/6.subnet-association.tf

--- terraform-files/terraform-fullcode/6.subnet-association.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
