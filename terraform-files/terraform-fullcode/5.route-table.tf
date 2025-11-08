3c15e141289c796ca0afe908bfed7b9b20fc21b7
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-08 13:53:47 +0530

    Update 5.route-table.tf

--- terraform-files/terraform-fullcode/5.route-table.tf
+++ terraform-files/terraform-fullcode/5.route-table.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table" "myrt" {
+resource "aws_route_table" "public_rt" {
   tags = {
     Name = "Terraform-RT"
   }
@@ -6,5 +6,16 @@ resource "aws_route_table" "myrt" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myigw.id
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.mynat.id
+  }
+  tags = {
+    Name = "terraform-private-rt"
   }
 }
