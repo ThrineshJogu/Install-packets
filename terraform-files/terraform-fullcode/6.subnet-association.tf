f832f0480adf7ed13dea3fc6bcf5431dfe293767
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-08 14:22:58 +0530

    Update 6.subnet-association.tf

--- terraform-files/terraform-fullcode/6.subnet-association.tf
+++ terraform-files/terraform-fullcode/6.subnet-association.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.subnet1.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.subnet2.id
+  subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
