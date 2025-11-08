cbe33a43d4958c19f953f6e5a0ecc6916b134251
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-08 15:14:14 +0530

    Delete terraform-files/terraform-fullcode/2.vpc.tf

--- terraform-files/terraform-fullcode/2.vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "myvpc" {
-  tags = {
-    Name = "Terraform-VPC"
-  }
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-}
