0ae8f73b2bd38e70e6e68b5c87bc2b325427ddfc
ThrineshJogu <joguthrinesh123@gmail.com>
2025-11-08 14:52:37 +0530

    Update public-public.tf

--- terraform-files/terraform-fullcode/public-public.tf
+++ terraform-files/terraform-fullcode/public-public.tf
@@ -128,18 +128,16 @@ resource "aws_security_group" "mysg" {
 # ------------------- Launch Template -------------------
 resource "aws_launch_template" "mylt" {
   name          = "Terraform-LT"
-  description   = "Created by Terraform"
+  description   = "This is created by terraform"
   image_id      = "ami-07fb0a5bf9ae299a4"
   instance_type = "t3.micro"
   key_name      = "newkey"
-
+  placement {
+    availability_zone = "eu-north-1a"
+  }
   vpc_security_group_ids = [aws_security_group.mysg.id]
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "MyServer"
-    }
+  tags = {
+    Name = "MyServer"
   }
 }
 
@@ -163,21 +161,14 @@ resource "aws_elb" "myelb" {
 
 # ------------------- Auto Scaling Group -------------------
 resource "aws_autoscaling_group" "myasg" {
-  name                 = "Terraform-ASG"
+  name = "Terraform-ASG"
+  launch_template {
+    id = aws_launch_template.mylt.id
+  }
   min_size             = 1
   max_size             = 6
   desired_capacity     = 2
   health_check_type    = "EC2"
   load_balancers       = [aws_elb.myelb.name]
   vpc_zone_identifier  = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-
-  launch_template {
-    id = aws_launch_template.mylt.id
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "Terraform-ASG"
-    propagate_at_launch = true
-  }
 }
